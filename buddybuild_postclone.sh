7d4fd79da95773ab2f26c89e99f73ad8d0fdafa9
Todd Anglin <toddanglin@gmail.com>
2017-01-19 23:24:38 -0600

    Add BuddyBuild compat script

---
+++ buddybuild_postclone.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Install NativeScript CLI
+echo "******** Install NativeScript CLI *************"
+npm install -g nativescript
+
+# NativeScript Ready
+echo "******** NativeScript CLI Ready *************"
+tns --version
+
+# Prepare NativeScript Project
+echo "******** Prepare NativeScript iOS Project *************"
+tns prepare ios
